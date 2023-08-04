ce8f2fbc8bebd63a3308945bbc50ff4baf99fb83
sakib biswas <sakibbiswas418@gmail.com>
2023-08-03 21:38:58 -0700

    single martial art page updated

--- app/martialarts/[id]/page.jsx
+++ app/martialarts/[id]/page.jsx
@@ -5,7 +5,7 @@ const page = ({ params }) => {
   const id = params.id;
 
   return (
-    <section className="bg-gray-800">
+    <section className="bg-gray-900">
       <div className="wrapper section-padding">
         <h1 className="uppercase text-3xl font-semibold text-accent1 mb-10">
           Discover the Power of Martial Arts!
@@ -13,17 +13,17 @@ const page = ({ params }) => {
         {martialArtsDetails.map(
           (art) =>
             art.id == id && (
-              <div className="grid grid-cols-2">
-                <div className="w-3/4 h-full mx-auto rounded-lg overflow-hidden ">
-                  <Image
-                    src={art.image}
-                    alt={art.name}
-                    width={800}
-                    height={600}
-                  />
-                </div>
-                <div className="p-5 bg-gray-900">
-                  <div className="text-light p-4 border border-gray-700 flex justify-around gap-5">
+              <div className="">
+                <div className="p-5 ">
+                  <div className="text-light p-4   flex justify-around gap-5">
+                    <div className=" h-full rounded-lg overflow-hidden ">
+                      <Image
+                        src={art.image}
+                        alt={art.name}
+                        width={400}
+                        height={400}
+                      />
+                    </div>
                     <div>
                       <p>{art.name}</p>
                       <p>origin: {art.origin}</p>
--- components/Button.jsx
+++ components/Button.jsx
@@ -3,18 +3,22 @@ import clsx from "clsx";
 import Link from "next/link";
 import React from "react";
 
-const buttonVariants = cva("px-4 py-3 font-semibold rounded-md duration-300", {
-  variants: {
-    color: {
-      green: "bg-accent1 text-dark hover:bg-accent1/90",
-      red: "bg-accent2 text-dark hover:bg-accent2/90",
-      outline: "bg-transparent text-light hover:bg-dark/10 border border-light",
+const buttonVariants = cva(
+  "px-4 py-3 font-semibold rounded-md duration-300 cursor-pointer",
+  {
+    variants: {
+      color: {
+        green: "bg-accent1 text-dark hover:bg-accent1/90",
+        red: "bg-accent2 text-dark hover:bg-accent2/90",
+        outline:
+          "bg-transparent text-light hover:bg-dark/10 border border-light",
+      },
     },
-  },
-  defaultVariants: {
-    color: "green",
-  },
-});
+    defaultVariants: {
+      color: "green",
+    },
+  }
+);
 
 const Button = ({ href, bText, target, color }) => {
   return (
